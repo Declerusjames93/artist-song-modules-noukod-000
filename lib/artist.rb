e8e4791e5df3d55ca2b40321ba1044bc1cab59d0
James Declerus <james.declerus@esih.edu>
2019-05-06 17:48:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,5 @@
 require 'pry'
-require_relative '../lib/concerns/findable'
-require_relative '../lib/concerns/memorable'
- require_relative '../lib/concerns/paramable'
+require_relative 'config/environment.rb'
 class Artist
   attr_accessor :name
   attr_reader :songs
